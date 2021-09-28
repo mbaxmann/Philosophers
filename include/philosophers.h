fbc6cd53d7fb53e632af341fee9f4e6a63a1aae5
mbaxmann <mathias.bamann4@gmail.com>
2021-09-28 21:12:43 +0200

    ..

---
+++ include/philosophers.h
@@ -0,0 +1,11 @@
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
